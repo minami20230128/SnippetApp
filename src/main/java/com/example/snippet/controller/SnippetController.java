c507a99af69d8d274123d33f51d98648793f80b1
minami20230128 <south614fruit@yahoo.co.jp>
2025-07-21 21:25:19 +0900

    更新画面いったん完成

--- src/main/java/com/example/snippet/controller/SnippetController.java
+++ src/main/java/com/example/snippet/controller/SnippetController.java
@@ -3,12 +3,17 @@ package com.example.snippet.controller;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.server.ResponseStatusException;
 
+import com.example.snippet.input.EditSnippetInput;
 import com.example.snippet.repository.SnippetRepository;
+
+import java.time.LocalDateTime;  
 
 @Controller
 @RequestMapping("/snippets")
@@ -32,5 +37,26 @@ public class SnippetController {
 	            .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Snippet not found"));
 		model.addAttribute("snippet", snippet);
 		return "detail";
+	}
+	
+	@GetMapping("/{id}/edit")
+	public String edit(@PathVariable int id, Model model) {
+	    var snippet = this.snippetRepository.findById(id)
+	            .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Snippet not found"));
+		model.addAttribute("snippet", snippet);
+		return "edit";
+	}
+	
+	@PutMapping("/{id}/edit")
+	public String edit(@Validated EditSnippetInput editSnippetInput,
+			@PathVariable int id, Model model) {
+	    var snippet = this.snippetRepository.findById(id)
+	            .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Snippet not found"));
+		snippet.setTitle(editSnippetInput.getTitle());
+		snippet.setCode(editSnippetInput.getCode());
+		snippet.setUpdatedAt(LocalDateTime.now());
+		this.snippetRepository.save(snippet);
+		
+		return "redirect:/snippets/";
 	}
 }
---
+++ src/main/java/com/example/snippet/input/EditSnippetInput.java
@@ -0,0 +1,50 @@
+package com.example.snippet.input;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+public class EditSnippetInput {
+	private Integer id;
+
+	@NotBlank
+	@Size(max=128)
+    private String title;
+
+	@NotBlank
+    private String code;
+
+	@NotBlank
+    private String description;
+	
+    public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getCode() {
+		return code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+}
