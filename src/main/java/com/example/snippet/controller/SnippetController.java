0e8c84e088d565c82ef039c98ac29827a7d44a91
minami20230128 <south614fruit@yahoo.co.jp>
2025-07-21 19:50:25 +0900

    詳細画面いったん完成

--- src/main/java/com/example/snippet/controller/SnippetController.java
+++ src/main/java/com/example/snippet/controller/SnippetController.java
@@ -1,9 +1,12 @@
 package com.example.snippet.controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.example.snippet.repository.SnippetRepository;
 
@@ -21,5 +24,13 @@ public class SnippetController {
 		var snippets = this.snippetRepository.findAll();
 		model.addAttribute("snippets", snippets);
 		return "index";
+	}
+	
+	@GetMapping("/{id}")
+	public String detail(@PathVariable int id, Model model) {
+	    var snippet = this.snippetRepository.findById(id)
+	            .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Snippet not found"));
+		model.addAttribute("snippet", snippet);
+		return "detail";
 	}
 }
